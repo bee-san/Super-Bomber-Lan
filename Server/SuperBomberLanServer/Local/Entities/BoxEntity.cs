4c1cafb7a1bd5cb1a3b0bf199b5706cdfce13abb
Mushroom <othergithub@watchingyour.tv>
2017-04-01 15:59:03 +0100

    Added box entity

---
+++ Server/SuperBomberLanServer/Local/Entities/BoxEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBomberLanServer.Local.Entities
+{
+    public class BoxEntity : IEntity
+    {
+        public EntityType EntityType { get; } = EntityType.Box;
+    }
+}
