80e72f4b9a89457e2fca02a1037231b6d23562af
Mushroom <othergithub@watchingyour.tv>
2017-04-01 17:25:52 +0100

    Added ASCII art on start up

--- Server/SuperBomberLanServer/Program.cs
+++ Server/SuperBomberLanServer/Program.cs
@@ -13,15 +13,32 @@ namespace SuperBomberLanServer
 
         public async Task Run()
         {
-            Console.WriteLine("-------------------------------------------------------------------------------------------");
-            Console.WriteLine("|                                         Welcome to                                      |");
-            Console.WriteLine("|                                     SUPER BOMBER LAN                                    |");
-            Console.WriteLine("-------------------------------------------------------------------------------------------\n\n");
+            Console.WriteLine(@"/-----------------------------------------------------------------------------------------\");
+            Console.WriteLine(@"|                                        Welcome to                                      |");
+            Console.WriteLine(@"|                               _____                                                    |");
+            Console.WriteLine(@"|                              / ____|                                                   |");
+            Console.WriteLine(@"|                             | (___  _   _ _ __   ___ _ __                              |");
+            Console.WriteLine(@"|                              \___ \| | | | '_ \ / _ \ '__|                             |");
+            Console.WriteLine(@"|                              ____) | |_| | |_) |  __/ |                                |");
+            Console.WriteLine(@"|                           __|_____/ \__,_| .__/ \___|_|                                |");
+            Console.WriteLine(@"|                          |  _ \          | |   | |                                     |");
+            Console.WriteLine(@"|                          | |_) | ___  _ _|_|__ | |__   ___ _ __                        |");
+            Console.WriteLine(@"|                          |  _ < / _ \| '_ ` _ \| '_ \ / _ \ '__|                       |");
+            Console.WriteLine(@"|                          | |_) | (_) | | | | | | |_) |  __/ |                          |");
+            Console.WriteLine(@"|                          |____/ \___/|_| |_| |_|_.__/ \___|_|                          |");
+            Console.WriteLine(@"|                                | |        /\   | \ | |                                 |");
+            Console.WriteLine(@"|                                | |       /  \  |  \| |                                 |");
+            Console.WriteLine(@"|                                | |      / /\ \ | . ` |                                 |");
+            Console.WriteLine(@"|                                | |____ / ____ \| |\  |                                 |");
+            Console.WriteLine(@"|                                |______/_/    \_\_| \_|                                 |");
+            Console.WriteLine("\\-----------------------------------------------------------------------------------------/\n\n");
             Console.WriteLine("Initialising....");
 
             //Generate the map
             Console.WriteLine("Generating map....");
             Map map = Map.CreateMap(64, 64);
+
+            //
             var test = JsonConvert.SerializeObject(map);
             Debugger.Break();
 
